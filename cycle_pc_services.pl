f71381c0b96874bdbd2105f8136d97933bef6116
kraznerj <jorikrasner@gmail.com>
2017-09-03 22:54:50 -0500

    Working on same script but trying out perl

---
+++ cycle_pc_services.pl
@@ -0,0 +1,34 @@
+####################################################
+#This script will cycle windows services via cygwin#
+####################################################
+
+############################################
+#####Check to make sure User is Oracle######
+############################################
+my $login = (getpwuid $>);
+die "must run as oracle" if $login ne 'oracle';
+
+###################
+###HELP Function###
+###################
+sub usage {
+    print <<HELP
+
+  \e[1m$0\e[0m
+
+  This script will cycle windows services via cygwin
+
+  \e[1mUSAGE:\e[0m
+
+    $0; <pc_name>
+    $0; <pc_name> <pc_name>
+
+HELP
+}
+if( $ARGV[0] eq '-h' || $ARGV[0] eq '-help')
+{
+  help();
+  die;
+}
+
+#Accept CLI args into array
