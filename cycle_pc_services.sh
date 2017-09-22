07d5bd5e42860f3374946bbdc0209a5045966e2e
kraznerj <jorikrasner@gmail.com>
2017-09-21 21:22:47 -0500

    Created options in menu

--- cycle_pc_services.sh
+++ cycle_pc_services.sh
@@ -36,13 +36,11 @@ function mysql_cycle {
 service="mysql"
 for pc in ${argsPCs[@]}
 do
-  ssh $sshuser@$pc << EOF
-  sc stop $service
+  ssh $sshuser@$pc sc stop $service
   sleep 7
-  sc start $service
+  ssh $sshuser@$pc sc start $service
   sleep 5
-  sc query $service
-  EOF
+  ssh $sshuser@$pc sc query $service
 done
 }
 
@@ -51,43 +49,47 @@ function ggsmgr_cycle {
 service="ggsmgr"
 for pc in ${argsPCs[@]}
 do
-  ssh $sshuser@$pc << EOF
-  sc stop $service
+  ssh $sshuser@$pc sc stop $service
   sleep 7
-  sc start $service
+  ssh $sshuser@$pc sc start $service
   sleep 5
-  sc query $service
-  EOF
+  ssh $sshuser@$pc sc query $service
 done
 }
 
-read -p "Would you like to cycle MYSQL on the PC? (y/n)" -n 1 answermysql
-echo "" #For formmatting purposes
-read -p "Would you like to cycle GGSMGR as well? (y/n)" -n 1 answerggsmgr
-echo "" #For formmatting purposes
-if [ $answermysql == "y" ] && [ $answerggsmgr == "y" ];
-then
-        mysql_cycle
-        ggsmgr_cycle
-        echo "${green}Service cycle complete${reset}"
 
-elif [ $answermysql == "y" ] && [ $answerggsmgr == "n" ]
-then
-        mysql_cycle
-        echo ""
-        echo "${green}MYSQL cycle complete${reset}"
-        echo "${red}GGSMGR will not be cycled${reset}"
-
-elif [ $answermysql == "n" ] && [ $answerggsmgr == "y" ];
-then
-        ggsmgr_cycle
-        echo ""
-        echo "${green}GGSMGR cycle complete${reset}"
-        echo "${red}MYSQL will not be cycled${reset}"
-else [ $answermysql == "n" ] && [ $answerggsmgr == "n" ];
-        echo ""
-        echo "${green}No Services will be cycled${reset}"
-        echo "Why did you run this if you ain't cycling anything"
-fi
+PS3='What Services would you like to cycle? '
+options=("MySQL" "GGSMGR" "Both GGSMGR & MySQL" "Quit")
+select opt in "${options[@]}"
+do
+	case $opt in
+		"MySQL")
+			mysql_cycle
+        		echo "${green}MySQL cycle complete${reset}"
+			      echo ""
+            break
+			;;
+		"GGSMGR")
+			ggsmgr_cycle
+			echo "${green}GGSMGR cycle complete${reset}"
+			echo ""
+      break
+			;;
+		"Both GGSMGR & MySQL")
+			mysql_cycle
+			ggsmgr_cycle
+			echo ""
+			echo "${green}Service cycle complete${reset}"
+			echo ""
+      break
+			;;
+		"Quit")
+			echo "${green}No Services will be cycled${reset}"
+			echo "Why did you run this if you ain't cycling anything"
+			break
+			;;
+		*) echo "invalid option";;
+	esac
+done
 
 exit 0
